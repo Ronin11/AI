c7b625361455a943332e2c0db13f1e05ea055a50
Nate Ashby <nate.ashby@L15502.local>
2015-11-18 11:42:45 -0700

    More tests

--- javascripts/main.js
+++ javascripts/main.js
@@ -4,6 +4,8 @@ $(document).ready(function () {
 	data = $.getJSON('https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json');
 	console.log(data);
 	console.log(data.readyState);
+	if(data.readyState == 1)
+		console.log(data.responseText);
 	$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (temp) {
 		console.log(temp)
 	});
