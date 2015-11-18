e02ff7cdc2778bbbb6994e9cd6b560d4f279b897
Ronin11 <nate@raddd.com>
2015-11-17 19:33:12 -0700

    more debugging

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,44 +1,34 @@
-var results;
 
-function loadDemo() {
+$(document).ready(function () {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 	if (xhttp.readyState == 4 && xhttp.status == 200) {
-		results = xhttp.responseText
+		var results = JSON.stringify({xhttp.responseText});
+		//$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        // Create the chart
+	        $('#demo').highcharts('StockChart', {
+
+	            rangeSelector : {
+	                selected : 1
+	            },
+
+	            title : {
+	                text : 'AI Account Value'
+	            },
+
+	            series : [{
+	                name : 'Value',
+	                data : results,
+	                tooltip: {
+	                    valueDecimals: 2
+	                }
+	            }]
+	        });
+	    //});
 		}
 	};
 	xhttp.open("GET", "https://raw.githubusercontent.com/Ronin11/AI/master/data/data.txt", true);
 	xhttp.send();
-}
-
-$(document).ready(function () {
-		//loadDemo();
-		//console.log(results);
-		myArray = $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?');//['response.JSON']
-		console.log(myArray);
-		$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
-        // Create the chart
-        console.log(data);
-        $('#demo').highcharts('StockChart', {
-
-            rangeSelector : {
-                selected : 1
-            },
-
-            title : {
-                text : 'AI Account Value'
-            },
-
-            series : [{
-                name : 'Value',
-                data : data,
-                tooltip: {
-                    valueDecimals: 2
-                }
-            }]
-        });
-    });
-
 });
 
 
