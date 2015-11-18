071ea84bbf0b578d4379219c0bb81b3df2cb0506
Ronin11 <nate@raddd.com>
2015-11-17 17:56:34 -0700

    Javascript test

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,32 @@
+$(document).ready(function () {
 console.log('This would be the main JS file.');
+}
+
+/*
+$(document).ready(function () {
+
+    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        // Create the chart
+        $('#container').highcharts('StockChart', {
+
+
+            rangeSelector : {
+                selected : 1
+            },
+
+            title : {
+                text : 'AAPL Stock Price'
+            },
+
+            series : [{
+                name : 'AAPL',
+                data : data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
+
+});
+*/
