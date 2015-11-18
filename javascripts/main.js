41614c1c674cbd9e8a6f057040e6c1192570175c
Ronin11 <nate@raddd.com>
2015-11-17 19:34:58 -0700

    more debugging

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,7 +3,8 @@ $(document).ready(function () {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 	if (xhttp.readyState == 4 && xhttp.status == 200) {
-		var results = JSON.stringify({xhttp.responseText});
+		var results = xhttp.responseText;//JSON.stringify({xhttp.responseText});
+		console.log(results)
 		//$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
         // Create the chart
 	        $('#demo').highcharts('StockChart', {
