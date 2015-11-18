f0f1edb6b623e5dc9afdd182ecd032693c10c110
Ronin11 <nate@raddd.com>
2015-11-17 18:47:42 -0700

    I believe in miracles

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,26 +1,37 @@
+var results;
+
+function loadDemo() {
+	var xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function() {
+	if (xhttp.readyState == 4 && xhttp.status == 200) {
+		results = xhttp.responseText
+		}
+	};
+	xhttp.open("GET", "https://raw.githubusercontent.com/Ronin11/AI/master/data/data.js", true);
+	xhttp.send();
+}
+
 $(document).ready(function () {
+		loadDemo();
+		// Create the chart
+		$('#demo').highcharts('StockChart', {
 
-    $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
-        // Create the chart
-        $('#demo').highcharts('StockChart', {
+		rangeSelector : {
+			selected : 1
+		},
 
+		title : {
+			text : 'AI Account Value'
+		},
 
-            rangeSelector : {
-                selected : 1
-            },
-
-            title : {
-                text : 'AAPL Stock Price'
-            },
-
-            series : [{
-                name : 'AAPL',
-                data : data,
-                tooltip: {
-                    valueDecimals: 2
-                }
-            }]
-        });
-    });
-
+		series : [{
+			name : 'Value',
+			data : results,
+			tooltip: {
+				valueDecimals: 2
+			}
+		}]
+	});
 });
+
+
