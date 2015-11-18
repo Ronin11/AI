6ba72e5a3dbc5ba07d398b61ece672bf541e533e
Ronin11 <nate@raddd.com>
2015-11-18 12:43:21 -0700

    WOOOO

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,40 @@
 $(document).ready(function () {
 
+	//data = JSON.parse($.getJSON('https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json').recievedtext);
+	/*$.get("https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json?callback=?", function(data){
+  		console.log("callback");
+  		data = removeComments(data);
+  		console.log(data);
+  		console.log(JSON.parse(data));
+	});
+	data = $.getJSON('https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json');
+	//console.log(data);*/
+	//console.log(Object.keys(data));
+	//console.log(JSON.parse(JSON.stringify(data)));
+	//if(data.readyState == 1)
+	//	console.log(data.responseText);
+	$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (temp) {
+		// Create the chart
+		$('#container').highcharts('StockChart', {
+
+			rangeSelector : {
+				selected : 1
+			},
+
+			title : {
+				text : 'AI Account Value'
+			},
+
+			series : [{
+				name : 'Value',
+				data : data,
+				tooltip: {
+					valueDecimals: 2
+				}
+			}]
+		});
+	});
+
 	$.get("https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json?callback=?", function(data){
 		data = removeComments(data);
 		// Create the chart
