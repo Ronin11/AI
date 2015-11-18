c63bfd0c74db565a1bfd178581939223aa14ab6d
Nate Ashby <nate.ashby@L15502.local>
2015-11-18 11:27:32 -0700

    More tests

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
 
-	data = $.getJSON('https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json');
+	data = JSON.parse($.getJSON('https://raw.githubusercontent.com/Ronin11/AI/master/data/data.json'));
 	console.log(data);
 	$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (temp) {
 		console.log(temp)
 	});
 	// Create the chart
-	$('#container').highcharts('StockChart', {
+	$('#demo').highcharts('StockChart', {
 
 		rangeSelector : {
 			selected : 1
@@ -25,40 +25,3 @@ $(document).ready(function () {
 		}]
 	});
 });
-
-/*
-$(document).ready(function () {
-	var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function() {
-	if (xhttp.readyState == 4 && xhttp.status == 200) {
-		var results = JSON.stringify(xhttp.responseText);
-		console.log(results)
-		//$.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
-		// Create the chart
-			$('#demo').highcharts('StockChart', {
-
-				rangeSelector : {
-					selected : 1
-				},
-
-				title : {
-					text : 'AI Account Value'
-				},
-
-				series : [{
-					name : 'Value',
-					data : results,
-					tooltip: {
-						valueDecimals: 2
-					}
-				}]
-			});
-		//});
-		}
-	};
-	xhttp.open("GET", "https://raw.githubusercontent.com/Ronin11/AI/master/data/data.txt", true);
-	xhttp.send();
-});
-
-
-*/
